2e7eda923c4e2f774baae056a760d11516fd3d8c
katiejg <jng.katie@gmail.com>
2018-07-14 15:35:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,7 @@
 # dog.rb
 
 class Dog 
-  def initialize(breed, name)
-    @breed = breed
-    @name = name
-  end
-  def breed=(breed)
-    @breed
-  end
-  def breed
-    @breed
-  end
-  def name=(name)
-    @name
-  end
-  def name
-    @name
-  end
+attr_accessor
 end 
 
 fido = Dog.new("Husky", "Fido")
