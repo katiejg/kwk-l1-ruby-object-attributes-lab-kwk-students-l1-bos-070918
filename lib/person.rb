816e58ed112a1700ae2d867d8705c4d5f22a1a06
katiejg <jng.katie@gmail.com>
2018-07-14 15:26:06 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,16 @@
 # person.rb
+
+class Person
+  def initialize(name)
+    @name
+  end
+  def name=(name)
+    @name
+  end
+  def name
+    @name
+  end
+end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
